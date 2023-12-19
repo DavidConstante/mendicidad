42f22e6d2ec01edd4a20c299e22675844d709645
DavidConstante <omileconstante@gmail.com>
2023-12-18 20:21:10 -0500

    FIX: change home page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,7 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
-import wallpaper from '../assets/wallpaper.jpg'
 import Header from '../components/Header'
-
+import video from '../assets/video.mp4'
 const HomePage = () => {
   return (
     <div >
@@ -13,15 +12,18 @@ const HomePage = () => {
       {/* Hero */}
       <div className='flex flex-col items-center bg-gradient-to-r from-bg to-purple-800 text-white'>
         <h1 className="mx-8 my-8 text-3xl md:text-4xl lg:text-7xl text-center font-bold  ">
-          {`Solidaridad sin Mendicidad`}
+          {`Derechos con Dignidad`}
         </h1>
 
 
-        <p className='px-8 text-xl text-justify lg:w-4/5' >
+        <p className='px-8 lg:px-0 text-xl text-justify lg:w-4/5' >
           {`Entregar donaciones en la vía pública es preligroso, puedes ponerte en contacto con las comunidades aledañas para que tú mismo entregues tus donaciones en un lugar seguro. Cuida tu integridad y la de los demás.`}
         </p>
+
         <br />
-        <p className='px-8 text-xl text-justify lg:w-4/5' >
+        <p className='px-8 lg:px-0 text-xl text-justify lg:w-4/5'>Evita multas y contravenciones</p>
+        <br />
+        <p className='px-8  lg:px-0 text-xl text-justify lg:w-4/5' >
           {`CÓDIGO ORGÁNICO INTEGRAL PENAL (COIP)`}
         </p>
 
@@ -31,13 +33,11 @@ const HomePage = () => {
 
 
 
-        <div className='static flex flex-col items-center lg:flex-row justify-center m-4 '>
+        <div className='static flex flex-col items-center lg:flex-row justify-center m-4 lg:w-4/5 '>
 
-          <img
-            className=' w-full lg:w-2/4 border-2 border-white rounded-xl'
-            src={wallpaper}
-            alt=""
-          />
+
+          <video controls className='w-3/5' src={video}></video>
+
           <div className='m-8'>
             <p className='text-2xl lg:text-4xl m-4 font-bold'>{`¿Qué se puede donar?`}</p>
             <h3 class="list-disc text-xl lg:text-2xl">
